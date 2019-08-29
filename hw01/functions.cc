8253970b1e674c5ba6b59f6a5972458c6827cbc0
Brett Warrender <bwarrend@my.hpu.edu>
2019-08-28 16:07:22 -1000

    hw01

--- hw01/functions.cc
+++ hw01/functions.cc
@@ -2,11 +2,16 @@
 #include <time.h>
 #include <stdlib.h>
 
+//Returns an random int between a and b
 int rRange(int a, int b){
+	//Seeds the RNG with the time
 	srand(time(NULL));
+	//Returns random number modulos the range + 1, then add
+	//the min, which is a
 	return (rand() % (b-(a-1))) + a;
 }
 
+//Prints Hello World horizontal if x > 5, else prints it vertically
 void printHW(int x){
 	if (x > 5)
 	{
