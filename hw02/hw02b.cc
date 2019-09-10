cbae5719b7c82bff11c171d8ee4f902384ffcecd
Brett W <bwarrend@my.hpu.edu>
2019-09-09 19:26:35 -1000

    HW02 for Wednesday's class

--- hw02/hw02b.cc
+++ hw02/hw02b.cc
@@ -1,6 +1,7 @@
 #include <cstdio>
 
-
+//Take two into pointers and swtich their int value,
+//..value change occurs in memory so no return needed.
 void swapInts(int* a, int* b)
 {
   int temp;
@@ -14,12 +15,12 @@ int main(int argc, char** argv)
 {
   int a;
   int b;
-
+  //Get us some user ints.
   printf("Enter 2 integers:\n");
   scanf("%d %d", &a, &b);
-
+  //This is what the user inputted in case they forgot.
   printf("a = %d\nb = %d\n", a, b);
-
+  //Run our function with user input and re-print out the numbers.
   swapInts(&a, &b);
   printf("Swapped:\n");
   printf("a = %d\nb = %d\n", a, b);
