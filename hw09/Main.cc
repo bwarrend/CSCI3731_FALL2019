57381c2a798c7ae4e4a3a914d2f56e6eb556e94f
Brett Warrender <bwarrend@my.hpu.edu>
2019-11-06 14:10:42 -1000

    for wed class

--- hw09/Main.cc
+++ hw09/Main.cc
@@ -16,63 +16,74 @@ int main(){
     //Seed rng set up some constant fish related paramters
     //
     srand(time(0));
-    Population fishList(100000);
+    const int MAX_POP = 20000;
+    Population fishList(MAX_POP);
     Population& flRef = fishList;
 
+    //Both fish initial values
+    //
     const int INIT_X = 0;
     const int INIT_Y = 0;
-
-    //const int SWIM_SP = 4;
-    //const int TURN_SP = 45;
-    
-    //const int INIT_DIR = 90;
-    
     const int UP_X_BOUND = INIT_X + 100;
     const int LOW_X_BOUND = INIT_X - 100;
     const int UP_Y_BOUND = INIT_Y + 100;
     const int LOW_Y_BOUND = INIT_Y - 100;
+    const int INIT_POPULATION = MAX_POP / 2;
 
-    double stepCount = 0;
-
-    //While loop so the population doesn't go completely out of control, will stop if too many fish are created
+    //FlippyFishOnly.com
     //
-    while(fishList.getPopCount() < fishList.getMaxFishes()){
-        //Make a new fish using the constants from above
-        //
-        
-        //new FlippyFish(INIT_X, INIT_Y, rRange(INIT_SWIM_SP_MIN, INIT_SWIM_SP_MAX), rRange(INIT_TURN_SP_MIN,INIT_TURN_SP_MAX), INIT_DIR, flRef);
-        new DrunkenFish(INIT_X, INIT_Y, 3, flRef);
+    const int SWIM_SP = 4;
+    const int TURN_SP = 45;    
+    const int INIT_DIR = 90;
 
-        //Print current steps, first fish x and y, and current population
-        //
-        //cls();
-        std::cout << "Step: " << ++stepCount << std::endl;
-        std::cout << "FIRST FISH - X: " << fishList.getFish(0)->getX() << " Y: " << fishList.getFish(0)->getY() << std::endl;        
-        std::cout << "Current population: " << fishList.getPopCount() << std::endl;
-        std::cout << "Ctrl+C to quit" << std::endl;
-        
-        //For loop over only the created fish, if they exist and are within bounds, run swim().  If OOBs, then delete.
-        //
-        int currentPop = fishList.getPopCount();// + 1;
-        for(int i = 0 ; i < currentPop; ++i){
+
+    int flippyCount = INIT_POPULATION-3000;
+
+    for (int i = 0; i < flippyCount; ++i){
+        new FlippyFish(INIT_X, INIT_Y, SWIM_SP, TURN_SP, INIT_DIR, flRef);    
+    }
+
+    
+    int drunkyCount = INIT_POPULATION;
+    for(int i = 0; i < drunkyCount; ++i){
+        new DrunkenFish(INIT_X, INIT_Y, SWIM_SP, flRef);
+    }
+
+
+
+    while(flippyCount > 0 && drunkyCount > 0){
+        for(int i = 0 ; i < MAX_POP; ++i){
             if(fishList.getFish(i) != nullptr){
                 Fish* f = fishList.getFish(i);
                
                 if(f->getX() > LOW_X_BOUND && f->getX() < UP_X_BOUND && f->getY() > LOW_Y_BOUND && f->getY() < UP_Y_BOUND){
                     f->swim();
                 }else{
-                    DrunkenFish* ff = dynamic_cast<DrunkenFish*>(f);
-
+                    FlippyFish* ff = dynamic_cast<FlippyFish*>(f);
                     if(ff != nullptr){
-                        std::cout << "FLIPPY FLAPPY";
-                    }
-
-                    
-                   
+                        --flippyCount;
+                    }else{
+                        --drunkyCount;
+                    }                   
                     delete fishList.getFish(i);
                 }
             }
         }
+
+        cls();
+        std::cout << "**FlippyFish VS DrunkenFish**" << std::endl;
+        std::cout << "Total Population: " << fishList.getPopCount() << std::endl;
+        std::cout << "FlippyFish Remaining: " << flippyCount << std::endl;
+        std::cout << "DrunkenFish Remaining: " << drunkyCount << std::endl;
+        
+    }
+
+    if(flippyCount > drunkyCount){
+        std::cout << std::endl << "Flippys are victorious!" << std::endl;
+    }else if(drunkyCount > flippyCount){
+        std::cout << std::endl << "Drunkys are victorious!" << std::endl;
+    }else{
+        std::cout << std::endl << "Miraculously, a draw.." << std::endl; 
     }
 
     return 0;
